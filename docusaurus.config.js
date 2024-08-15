73fd76f9e19a6c7327849988a50a03e8ae28ac44
Liam-Li <haozheng.l@outlook.com>
2024-08-15 00:22:38 -0400

    chore: update baseUrl in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   url: "https://onboarding-solutions.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/Onboarding-Portal-Docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
